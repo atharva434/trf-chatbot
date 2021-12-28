5a25a9deb2e74aadcca5bc26a4ab5a720787ee01
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-12-28 14:03:12 +0530

    chattgit add .!

---
+++ predict/views.py
@@ -0,0 +1,46 @@
+from django.http import response
+from django.shortcuts import render
+import pickle
+import numpy as np
+from keras.models import load_model
+from keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+# Create your views here.
+label_encoder = LabelEncoder()
+
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def chatbot(request):
+    if request.method == 'GET':            
+        inp= request.GET.get('inp')
+    with open("models\pew_intents.json") as file:
+        data = json.load(file)
+    chat_model = load_model('models\estv2.h5')
+    # load tokenizer object
+    with open('models\pickles\okenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('models\pickles\lbl_encoder.pickle', 'rb') as enc:
+        onehot_encoded = pickle.load(enc)
+    max_len = 10
+    result = chat_model.predict(pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+    print(result)
+    print(np.argmax(result))
+    tag = onehot_encoded.inverse_transform([np.argmax(result)])
+        
+
+    for i in data['intents']:
+        if i['tag'] == tag:
+            a=np.random.choice(i['responses'])
+            return response.JsonResponse(a,safe=False)
+# def display(request):
+
+#     return response.JsonResponse(prediction_label,safe=False)
